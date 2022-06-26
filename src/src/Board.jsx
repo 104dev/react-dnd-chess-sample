25416276cb1e2b726caead2b637275d2dfc31bbc
104 <104developers@gmail.com>
2022-06-27 00:23:25 +0900

    moving Knight to a Square that was clicked

--- src/src/Board.jsx
+++ src/src/Board.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import Square from './Square'
 import Knight from './Knight'
+import { moveKnight } from './Game'
 
+//現状のRenderSquareはループ回数とコマのX軸、Y軸を渡す。
+//ループ中にコマのX軸とY軸が一致したらKnightをレンダリングする。
+//Squareコンポーネントはその位置情報をレンダリング時に知っておく必要はない。
+
+/*
 function renderSquare(i, [knightX, knightY]) {
     const x = i % 8
     const y = Math.floor(i / 8)
@@ -14,6 +20,21 @@ function renderSquare(i, [knightX, knightY]) {
             <Square black={black}>{piece}</Square>
         </div>
     )
+}
+*/
+
+function renderSquare(i, [knightX, knightY]) {
+    const x = i % 8
+    const y = Math.floor(i / 8)
+    const isKnightHere = x === knightX && y === knightY
+    const black = (x + y) % 2 === 1
+    const piece = isKnightHere ? <Knight /> : null
+
+    return <div onClick={() => handleSquareClick(x, y)} style={{ width: '12.5%', height: '12.5%' }}>{<Square black={black}>{piece}</Square>}</div>
+}
+
+function handleSquareClick(toX, toY) {
+    moveKnight(toX, toY)
 }
 
 export default function Board({ knightPosition }) {
--- src/src/Game.jsx
+++ src/src/Game.jsx
@@ -1,11 +1,16 @@
-export function observe(receive) {
+
+/*export function observe(receive) {
     const randPos = () => Math.floor(Math.random() * 8)
     setInterval(() => receive([randPos(), randPos()]), 500)
-}
+}*/
 
 //クリックしたSquareにKnightを移動させることがゴール。
 //1つの方法としてはSquareから直接Gameロジックを呼ぶ方法がある。
 //しかしSquareにその位置情報を渡さなければならない。
+
+//moveKnightを定義。コマを移動した際に呼び出す。
+//移動先の座標を引数として取り、現在最初に初期値のknightPositionを書き換える。
+//emitChangeを呼び出す
 
 
 let knightPosition = [0, 0]
